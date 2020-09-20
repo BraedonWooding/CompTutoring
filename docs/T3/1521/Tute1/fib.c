bbb1bb5ce4c42348cd50d8d06280df150063af30
Braedon Wooding <braedonww@gmail.com.au>
2020-09-20 14:58:36 +1000

    Fib

--- docs/T3/1521/Tute1/fib.c
+++ docs/T3/1521/Tute1/fib.c
@@ -8,11 +8,11 @@
                            /       \
                           /         \
                          /           \
-                       fib(4)          fib(3)------
-                      /       \              /     \
-                   fib(3)      fib(2)        fib(2) fib(1)
-                  /   \         /  \           /  \
-                fib(2)fib(1)  fib(1) fib(0) fib(1) fib(0)
+                       fib(4)          ------fib(3)---
+                      /       \                /      \
+                   fib(3)      fib(2)         fib(2)  fib(1)
+                  /   \         /     \        /   \
+                fib(2) fib(1)  fib(1) fib(0) fib(1) fib(0)
                 /    \
               fib(1) fib(0)
 
