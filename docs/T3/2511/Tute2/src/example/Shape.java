a264f99fdcfa6fc45d2acdcf27f82d044be56db9
Braedon Wooding <braedonww@gmail.com.au>
2020-09-21 14:59:36 +1000

    remove contents (since I will cover them)

--- docs/T3/2511/Tute2/src/example/Shape.java
+++ docs/T3/2511/Tute2/src/example/Shape.java
@@ -1,26 +1,5 @@
 package example;
 
 public class Shape {
-    private String color;
 
-    public String GetColor() {
-        return this.color;
-    }
-
-    public int GetArea() {
-        return 0;
-    }
-
-    public static Shape GetRedShape() {
-        return new Shape("red");
-    }
-
-    public Shape(String color) {
-        System.out.println("Inside Shape constructor");
-        this.color = color;
-    }
-
-    public static void main(String[] args) {
-        Shape redShape = Shape.GetRedShape();
-    }
 }
