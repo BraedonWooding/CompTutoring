b208cee306506098d1d5a75d8e13d821576b246d
Braedon Wooding <braedonww@gmail.com.au>
2020-11-03 15:19:42 +1100

    Finished

--- docs/T3/1521/Tute8/byte_splice.c
+++ docs/T3/1521/Tute8/byte_splice.c
@@ -9,6 +9,10 @@ void usage(void) {
     );
 }
 
+int minimum(int a, int b) {
+    return a < b ? a : b;
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2 || (argc - 2) % 3 != 0) {
         fprintf(stderr, "Invalid Arguments...\n");
@@ -18,10 +22,71 @@ int main(int argc, char *argv[]) {
 
     char *filename = argv[1];
     int i = 2;
-    int f_in = open(filename, O_RDONLY);
-    if (f_in < 0) {
+    FILE *f_in = fopen(filename, "rb");
+    if (!f_in) {
         perror(filename);
         exit(1);
+    }
+    
+    for (int i = 2; i < argc; i += 3) {
+        char *endptr = NULL;
+        long start = strtol(argv[i], &endptr, 10);
+        if (!endptr || *endptr != '\0') {
+            printf("%s is not a number\n", argv[i]);
+            usage();
+            exit(1);
+        }
+        
+        endptr = NULL;
+        long end = strtol(argv[i + 1], &endptr, 10);
+        if (!endptr || *endptr != '\0') {
+            printf("%s is not a number\n", argv[i + 1]);
+            usage();
+            exit(1);
+        }
+        
+        char *filename_out = argv[i + 2];
+        FILE *f_out = fopen(filename_out, "wb");
+        if (f_out == NULL) {
+            perror(filename_out);
+            usage();
+            exit(1);
+        }
+        
+        fseek(f_in, start, SEEK_SET);
+        // 5000 to read
+        // 5000 - 1024 = 3976 - 1024 = 2952 - 1024 = 1928 - 1024
+        //             = 904 - 1024 = 0
+        int amount_to_read = end - start;
+        char buf[1024];
+        int bytes = 0;
+
+        // buffer is only 1024 bytes big
+        // so don't try to read more than 1024
+        // but also only read the minimum(amount_to_read, 1024)
+        while (amount_to_read > 0 &&
+              (bytes = fread(buf, 1, minimum(amount_to_read, 1024), f_in)) > 0) {
+            fwrite(buf, 1, bytes, f_out);
+            amount_to_read -= bytes;
+        }
+        
+        // int cur = 1;
+        // while (cur < start) {
+        //     // 0 <-> 255
+        //     // also -1
+        //     int c = fgetc(f_in);
+        //     if (c == EOF) break;
+        //     cur++;
+        // }
+        
+        // while (cur < end) {
+        //     int c = fgetc(f_in);
+        //     if (c == EOF) break;
+        //     cur++;
+        //     fputc(c, f_out);
+        // }
+
+        fclose(f_out);
     }
     
     close(f_in);
--- docs/T3/1521/Tute8/file_splice.c
+++ docs/T3/1521/Tute8/file_splice.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// "1"
+// Filename: "1"
 // hey
 // cats
 // what's
@@ -10,16 +10,16 @@
 
 // ./file_splice "./1" 1 3 "./o1" 1 4 "./o2" 5 6 "./o3"
 
-// "o1"
+// Filename: "o1"
 // hey
 // cats
 
-// "o2"
+// Filename: "o2"
 // hey
 // cats
 // what's
 
-// "o3"
+// Filename: "o3"
 // dawgs
 
 void usage(void) {
@@ -42,6 +42,54 @@ int main(int argc, char *argv[]) {
         perror(filename_in);
         usage();
         exit(1);
+    }
+    
+    for (int i = 2; i < argc; i += 3) {
+        char *endptr = NULL;
+        long start = strtol(argv[i], &endptr, 10);
+        if (!endptr || *endptr != '\0') {
+            printf("%s is not a number\n", argv[i]);
+            usage();
+            exit(1);
+        }
+        
+        endptr = NULL;
+        long end = strtol(argv[i + 1], &endptr, 10);
+        if (!endptr || *endptr != '\0') {
+            printf("%s is not a number\n", argv[i + 1]);
+            usage();
+            exit(1);
+        }
+        
+        char *filename_out = argv[i + 2];
+        FILE *f_out = fopen(filename_out, "w");
+        if (f_out == NULL) {
+            perror(filename_out);
+            usage();
+            exit(1);
+        }
+        
+        int cur = 1;
+        while (cur < start) {
+            // 0 <-> 255
+            // also -1
+            int c = fgetc(f_in);
+            if (c == '\n') cur++;
+            else if (c == EOF) break;
+        }
+        
+        while (cur < end) {
+            int c = fgetc(f_in);
+            if (c == '\n') cur++;
+            else if (c == EOF) break;
+
+            if (cur < end) {
+                fputc(c, f_out);
+            }
+        }
+
+        rewind(f_in);
+        fclose(f_out);
     }
     
     fclose(f_in);
