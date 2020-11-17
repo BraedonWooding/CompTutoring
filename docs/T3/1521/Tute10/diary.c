5715b4264aed7b38abdc7110dab63970a62bb504
Braedon Wooding <braedonww@gmail.com.au>
2020-11-17 12:34:01 +1100

    Tute10 ready

---
+++ docs/T3/1521/Tute10/diary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ./diary
+// ./diary --file $HOME/.diary
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
---
+++ docs/T3/1521/Tute10/print_file_bits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ./diary
+// .diary $HOME/.diary
+
+/*
+.data
+prompt:
+    .asciiz "Hello World\n"
+*/
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
