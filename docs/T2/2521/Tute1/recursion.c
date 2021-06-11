5ff9bc8f29a69c885a1efed8eb2785f89fb4e41c
Braedon Wooding <braedonww@gmail.com>
2021-06-11 12:43:47 +1000

    Done

--- docs/T2/2521/Tute1/recursion.c
+++ docs/T2/2521/Tute1/recursion.c
@@ -16,13 +16,67 @@ struct LL {
 };
 
 int len_while(struct LL *cur) {
+    int len = 0;
+    while (cur != NULL) {
+        len++;
+        cur = cur->next;
+    }
+    return len;
 }
 
 int len(struct LL *cur) {
+    // if (cur == NULL) {
+    //     return 0;
+    // } else if (cur->next == NULL) {
+    //     return 1;
+    // } else if (cur->next->next == NULL) {
+    //     return 2;
+    // } ...
+
+    if (cur == NULL) {
+        return 0;
+    }
+
+    return len(cur->next) + 1;
 }
 
-int sum_while(struct LL *cur) {
+int count_odds_while(struct LL *cur) {
+    int count = 0;
+    while (cur != NULL) {
+        if (cur->data % 2 != 0) {
+            count++;
+        }
+        cur = cur->next;
+    }
+    return count;
 }
 
-int sum(struct LL *cur) {
+int count_odds(struct LL *cur) {
+    // if (cur == NULL) {
+    //     return 0;
+    // } else if (cur->next == NULL) {
+    //     if (cur->data % 2 != 0) {
+    //         return 1;
+    //     } else {
+    //         return 0;
+    //     }
+    // } else if (cur->next->next == NULL) {
+    //     if (cur->data % 2 != 0) {
+    //         if (cur->next->data % 2 != 0) {
+    //             return 2;
+    //         } else {
+    //             return 1;
+    //         }
+    //     } else {
+    //         if (cur->next->data % 2 != 0) {
+    //             return 1;
+    //         } else {
+    //             return 0;
+    //         }
+    //     }
+    // }
+
+    if (cur == NULL) return 0;
+    return (cur->data % 2 != 0 ? 1 : 0) +
+        count_odds(cur->next);
 }
