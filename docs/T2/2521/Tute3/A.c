407d1ff2c42ec0e8f69e29c9bf8b2b173b7621dd
Braedon Wooding <braedonww@gmail.com>
2021-06-17 17:32:13 +1000

    Fix vec

---
+++ docs/T2/2521/Tute3/A.c
@@ -0,0 +1,11 @@
+#include "A.h"
+
+int cat(void) {
+    return 0;
+}
+
+int foo(void) {
+    return 10;
+}
+
+#include "A.h"
---
+++ docs/T2/2521/Tute3/A.h
@@ -0,0 +1,11 @@
+#ifndef A_H
+#define A_H
+
+int cat(void);
+int foo(void);
+
+#define A 10
+
+#include "B.h"
+
+#endif // A_H
