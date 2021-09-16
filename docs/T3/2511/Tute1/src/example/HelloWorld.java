0f4b9e84f981fef1d66fab6c5f1d35e4f286a6c0
Braedon Wooding <braedonww@gmail.com>
2021-09-16 13:52:25 +1000

    Fixup wk01

--- docs/T3/2511/Tute1/src/example/HelloWorld.java
+++ docs/T3/2511/Tute1/src/example/HelloWorld.java
@@ -4,7 +4,17 @@ package example;
  * Prints "Hello World" to the console.
  */
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static String getWelcomeMsg() {
+        return "Hello World";
+    }
+
+    public static void outputWelcomeMsg(String s) {
+        System.out.println(s);
+    }
+
+    public static void entry() {
+        // ...
+        String str = getWelcomeMsg();
+        outputWelcomeMsg(str);
     }
 }
